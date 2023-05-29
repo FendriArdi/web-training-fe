ee475ef7441c14b49c7eb413e28555c3950f711c
Rian Febriansyah <rianfebriansyah22@gmail.com>
2023-05-29 22:13:22 +0700

    feat: initialize tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "24px",
+      },
+      colors: {
+        primary: {
+          5: "#035E57",
+          4: "#017E75",
+          3: "#3EA799",
+          2: "#7CC1BC",
+          1: "#E7F6F6",
+        },
+        neutral: {
+          5: "#131313",
+          4: "#2B2828",
+          3: "#616161",
+          2: "#D0D0D0",
+          1: "#FBFBFB",
+        },
+      }
+    },
   },
   plugins: [],
 }
